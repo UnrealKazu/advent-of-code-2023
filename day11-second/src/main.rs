eee3d0e0a57e25b9fbd932d9334ad5a9a8f3584a
UnrealKazu <9115757+UnrealKazu@users.noreply.github.com>
2023-12-11 15:50:56 +0100

    Add WIP solution for day 11 part 2
    Remaining change: map should be refactored from a vector of vectors to a static array

---
+++ day11-second/src/main.rs
@@ -0,0 +1,243 @@
+use std::{
+    fmt,
+    fs::File,
+    io::{self, BufRead},
+    path::Path,
+    time::Instant,
+};
+
+use regex::Regex;
+
+const EXPANSION_RATE: usize = 100_000;
+
+#[derive(Clone, Copy)]
+struct SuperChar {
+    char: char,
+    empty: bool,
+}
+
+struct Universe {
+    map: Vec<Vec<SuperChar>>,
+    galaxies: Vec<Galaxy>,
+}
+
+struct Galaxy {
+    _id: u16,
+    position: Position,
+}
+
+struct Position {
+    x: usize,
+    y: usize,
+}
+
+impl fmt::Display for Universe {
+    // writes the map such that it looks like the same image as the input
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        for v1 in &self.map {
+            for v2 in v1 {
+                if v2.empty {
+                    write!(f, "X")?;
+                } else {
+                    write!(f, "{}", v2.char)?;
+                }
+            }
+            writeln!(f)?;
+        }
+
+        Ok(())
+    }
+}
+
+impl Universe {
+    // loop over all lines and expand them (i.e. double them) if they're empty
+    fn expand_empty_lines(&mut self) {
+        let empty_reg = Regex::new(r"^\.+$").unwrap();
+
+        let mut new_map: Vec<Vec<SuperChar>> = vec![];
+
+        for line in &self.map {
+            let mut line_clone = line.clone();
+            let str_line: String = line.iter().map(|l| l.char).collect();
+            if empty_reg.is_match(&str_line) {
+                // line is empty, so mark all SuperChar as such
+                for l in line_clone.iter_mut() {
+                    l.empty = true;
+                }
+            }
+            new_map.push(line_clone);
+        }
+
+        self.map = new_map;
+    }
+
+    fn transpose_map(&mut self) {
+        assert!(!self.map.is_empty());
+        self.map = (0..self.map[0].len())
+            .map(|i| {
+                self.map
+                    .iter()
+                    .map(|inner| inner[i])
+                    .collect::<Vec<SuperChar>>()
+            })
+            .collect()
+    }
+
+    fn store_galaxies(&mut self) {
+        let mut i = 0;
+        for (x, vec) in self.map.iter().enumerate() {
+            for (y, c) in vec.iter().enumerate() {
+                if c.char == '#' {
+                    self.galaxies.push(Galaxy {
+                        _id: i + 1,
+                        position: Position { x, y },
+                    });
+
+                    i += 1;
+                }
+            }
+        }
+    }
+
+    fn get_sum_shortest_paths(&self) -> usize {
+        let mut sum_min_length = 0;
+
+        // let path_length = |gal1: &Galaxy, gal2: &Galaxy| {
+        //     usize::abs_diff(gal1.position.x, gal2.position.x)
+        //         + usize::abs_diff(gal1.position.y, gal2.position.y)
+        // };
+
+        // for (i, gal1) in self.galaxies.iter().enumerate() {
+        //     for gal2 in self.galaxies.iter().skip(i + 1) {
+        //         sum_min_length += path_length(gal1, gal2);
+        //     }
+        // }
+
+        for (i, gal1) in self.galaxies.iter().enumerate() {
+            for gal2 in self.galaxies.iter().skip(i + 1) {
+                // loop over the entire x1 to x2 and y1 to y2 to check if there are empty cells
+                // TODO: fix the shortest path loop
+            }
+        }
+
+        sum_min_length
+    }
+}
+
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where
+    P: AsRef<Path>,
+{
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
+
+fn parse_input(path: &str) -> Universe {
+    let mut universe = Universe {
+        map: vec![],
+        galaxies: vec![],
+    };
+
+    if let Ok(lines) = read_lines(path) {
+        let empty_reg = Regex::new(r"^\.+$").unwrap();
+
+        for line_raw in lines {
+            let line = line_raw.unwrap();
+
+            let empty = empty_reg.is_match(&line);
+            universe
+                .map
+                .push(line.chars().map(|l| SuperChar { char: l, empty }).collect());
+        }
+    }
+
+    universe.transpose_map();
+    universe.expand_empty_lines();
+    universe.transpose_map();
+
+    universe.store_galaxies();
+
+    universe
+}
+
+fn main() {
+    // let now = Instant::now();
+
+    let universe = parse_input("./example.input");
+
+    println!("Resulting universe");
+    println!("{}", universe);
+
+    // let sum = universe.get_sum_shortest_paths();
+
+    // println!("Sum of shortest pair paths is {}", sum);
+
+    // println!("Duration: {}", now.elapsed().as_micros());
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn get_test_universe() -> Universe {
+        Universe {
+            map: vec![
+                vec![
+                    '.', '.', '.', '.', '#', '.', '.', '.', '.', '.', '.', '.', '.',
+                ],
+                vec![
+                    '.', '.', '.', '.', '.', '.', '.', '.', '.', '#', '.', '.', '.',
+                ],
+                vec![
+                    '#', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.',
+                ],
+                vec![
+                    '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.',
+                ],
+                vec![
+                    '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.',
+                ],
+                vec![
+                    '.', '.', '.', '.', '.', '.', '.', '.', '#', '.', '.', '.', '.',
+                ],
+                vec![
+                    '.', '#', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.',
+                ],
+                vec![
+                    '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '#',
+                ],
+                vec![
+                    '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.',
+                ],
+                vec![
+                    '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.',
+                ],
+                vec![
+                    '.', '.', '.', '.', '.', '.', '.', '.', '.', '#', '.', '.', '.',
+                ],
+                vec![
+                    '#', '.', '.', '.', '.', '#', '.', '.', '.', '.', '.', '.', '.',
+                ],
+            ],
+            galaxies: vec![],
+        }
+    }
+
+    #[test]
+    fn test_get_galaxies() {
+        let mut universe = get_test_universe();
+        universe.store_galaxies();
+
+        assert_eq!(universe.galaxies.len(), 9);
+    }
+
+    #[test]
+    fn test_get_pair_path() {
+        let mut universe = get_test_universe();
+        universe.store_galaxies();
+
+        let min_sum = universe.get_sum_shortest_paths();
+
+        assert_eq!(min_sum, 374);
+    }
+}
